f36e2da7a17866a4fa4bc5a0626098e8771f6b61
askl56 <amscott@openmailbox.org>
2017-05-21 17:21:45 +0100

    Add bar chart per postcode

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'residential/index'
   get 'residential/data', defaults: { format: 'json' }
+
+  get 'residential/bar_chart'
+  get 'residential/bar_data', defaults: { format: 'json' }
+  
   root to: 'residential#index'
 end
