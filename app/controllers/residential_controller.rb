a44714ef084bc6ba9816ea90c7b892fc07e5828c
askl56 <amscott@openmailbox.org>
2017-05-21 17:45:16 +0100

    Add first pass scatter graph

--- app/controllers/residential_controller.rb
+++ app/controllers/residential_controller.rb
@@ -8,12 +8,21 @@ class ResidentialController < ApplicationController
   end
 
   def bar_chart; end
-  
+
   def bar_data
     bar_data = SalesFigure.
       select(:id, :zipcode, :median_value).
       where(jurisdiction: 'Baltimore').
       order('median_value DESC, zipcode')
     render json: { bar_data: bar_data }
+  end
+
+  def scatter_chart; end
+  
+  def scatter_data
+    data = SalesFigure.
+      select(:id, :zipcode, :jurisdiction, :median_value, :total_sales).
+      order(:jurisdiction)
+    render json: { scatter_data: data }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get 'residential/bar_chart'
   get 'residential/bar_data', defaults: { format: 'json' }
+
+  get 'residential/scatter_graph'
+  get 'residential/scatter_data', defaults: { format: 'json' }
   
   root to: 'residential#index'
 end
